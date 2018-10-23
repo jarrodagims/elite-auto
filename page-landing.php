6e92a18864d9d205ec08753b390d3fe796f9a1c3
Jarrod Medrano <jmedran@gmail.com>
2018-10-23 11:35:44 -0600

    styling cards on landing page

---
+++ page-landing.php
@@ -0,0 +1,206 @@
+<?php
+/*
+    Template Name: Landing
+*/
+
+get_header(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="jumbo"><h1>Bob-O's is El Paso's <span class="br-md"></span><span class="red">#1 Party Venue</span> | Book Birthday Parties with Us!</span></h1>
+
+            <p>If you’re looking for a way to plan the perfect birthday party in El Paso, Bob-O’s Family Fun Center can help! Let our team take care of everything so you can have as much fun as possible. Bob-O’s is designed to provide unlimited fun to anyone and everyone. From laser tag and go-karts to an incredible arcade and batting cages, we truly have it all! We also offer excellent birthday party packages to make the planning process as effortless as possible. Make Bob-O’s Family Fun Center your birthday party headquarters!
+            </p>
+
+            <a href="https://bobosfun.a.pcsparty.com/bookings/?_ga=2.117101574.1839719463.1539814279-1322542885.1535553428" target="_blank"><button class="btn btn-primary">BOOK NOW!</button></a></div>
+        </div>
+    </div>
+</div>
+
+<section class="landing-module-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="cards-header"><h2>All packages are for <strong>10 CHILDREN</strong> and <br />one birthday child is <strong>FREE</strong></h2></div>
+
+                <div class="container">
+                    <div class="row flex-row">
+                        <div class="column">
+                            <div class="card">
+                                <div class="card-title">
+                                    <h3>Package I</h3>
+                                    <p>Basic Party</p>
+                                </div>
+
+                                <div class="card-text">
+                                    <p><strong class="price">$169.00</strong>  for the first 11<br />
+                                        <strong>$15 each</strong> additional guest</p>
+                                </div>
+
+                                <div class="card-ribbon">Each Guest Receives:</div>
+
+                                <div class="card-list">
+                                    <ul>
+                                        <li>12 Game Tokens</li>
+                                        <li>3 Outside Attractions*</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="card">
+                                <div class="card-title">
+                                    <h3>Package II</h3>
+                                    <p>Do-it-all Party</p>
+                                </div>
+
+                                <div class="card-text">
+                                    <p><strong class="price">$169.00</strong>  for the first 11<br />
+                                        <strong>$15 each</strong> additional guest</p>
+                                </div>
+
+                                <div class="card-ribbon">Each Guest Receives:</div>
+
+                                <div class="card-list">
+                                    <ul>
+                                        <li>12 Game Tokens</li>
+                                        <li>3 Outside Attractions*</li>
+                                        <li>2 Laser Odyssey Missions</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="column">
+                            <div class="card">
+                                <div class="card-title">
+                                    <h3>Package III</h3>
+                                    <p>Ultimate Party</p>
+                                </div>
+
+                                <div class="card-text">
+                                    <p><strong class="price">$299.00</strong>  for the first 11<br />
+                                        <strong>$21 each</strong> additional guest</p>
+                                </div>
+
+                                <div class="card-ribbon">Each Guest Receives:</div>
+
+                                <div class="card-list">
+                                    <ul>
+                                        <li>12 Game Tokens</li>
+                                        <li>2 Hours of Unlimited</li>
+                                        <li>Outside Attractions*</li>
+                                        <li>1 Laser Odyssey Mission</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row flex-row">
+                        <div class="column">
+                            <strong class="header">An attraction includes:</strong><br />
+                            <ul>
+                                <li>Go-Karts, Mini Go-Karts, Bumper</li>
+                                <li>Boats, Mini Golf, Dizzy Drums,</li>
+                                <li>Flying Banana, & Train Ride.</li>
+                            </ul>
+
+                            <a href="#">
+                                <button class="btn btn-primary"> Book Now!</button>
+                            </a>
+                        </div>
+                        <div class="column">
+                            <strong class="header">An attraction includes:</strong>
+
+                            <ul>
+                                <li>Go-Karts, Mini Go-Karts, Bumper</li>
+                                <li>Boats, Mini Golf, Dizzy Drums,</li>
+                                <li>Flying Banana, & Train Ride.</li>
+                            </ul>
+                        </div>
+                        <div class="column">
+                            <div class="card-bottom">
+                                <p><strong class="header">An attraction includes:</strong>
+                            Go-Karts, Mini Go-Karts, Bumper
+                            Boats, Mini Golf, Dizzy Drums, Flying
+                            Banana, & Train Ride.<br /><br />
+                                
+
+                            <strong>1 Laser Odyssey T-Shirt for Birthday <br />
+                                Child (One Per Package)</strong></p>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="ribbon">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>
+                    All Party Packages Include:
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="landing-module-2">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p><strong>3 XL One Topping Pizzas</strong>
+                <span>(For Paid Party Guests)</span></p>
+
+                <p><strong>Unlimited Fountain Drinks</strong><br />
+                <span>(For Paid Party Guests)</span></p>
+
+                <strong>A Party Host To Serve & Assist <br />Space Reserved For 2 Hours</strong>
+
+                <strong>$25 Non-refundable Deposit Required</strong>
+                <p>All Prices Subject To Change / Sales Tax<br />
+                Included In All Prices</p>
+
+            </div>
+
+            <div class="col">
+                <strong>With any birthday party package<br />you may add the following:</strong>
+
+                <p>Autograph Birthday Cake w/Sharpie
+                <span>$10.00 each</span></p>
+
+                <p>Laser Odyssey Missions
+                <span>$4.50 each</span></p>
+
+                <p>Laser Odyssey T-Shirt
+                <span>$13.00 each</span></p>
+            </div>
+
+            <div class="col">
+                <strong>For the Adults:</strong>
+                <p>XL One Topping Pizzas
+                <span>$14.00 each </span><br />
+                <span>$1.00 each additional topping</span></p>
+
+                <p>Large Cups (refillable)
+                <span>$2.50 each </span></p>
+
+                <p>Small Cups (refillable)
+                <span>$2.00 each</span></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
